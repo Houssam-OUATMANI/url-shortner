a2093b61234f35d94a23a8a43a2996632afa8fbc
Houssam Ouatmani <60886381+proservices-web@users.noreply.github.com>
2021-01-01 23:42:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const routes = require('./controllers/controller')
 const helmet = require('helmet')
 const morgan = require('morgan')
 
-
 app.set('view engine' ,'ejs')
 app.use(helmet())
 app.use(morgan('tiny'))
@@ -14,11 +13,5 @@ app.use(express.static('./public'))
 app.use(express.json())
 app.use(express.urlencoded({extended :true}))
 app.use(routes)
-
-
-
-
-
-
 
 app.listen(port)
